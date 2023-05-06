49d672992e101dd3c9f32711f063571fb5c6a031
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-06 21:25:39 +0700

    Update components and main page

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+
+const Header = () => {
+	return (
+		<div className="bg-red-500">
+			<Link to="/">
+				header
+			</Link>
+		</div>
+	)
+}
+
+export default Header
---
+++ client/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Header } from "./Header"
+export { default as Swap } from "./Swap"
